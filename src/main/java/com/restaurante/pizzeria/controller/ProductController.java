e693bb6b3b138bfa4b1ed4cd735de443de8f27d1
zareangel <miguelaz1@outlook.com>
2026-04-27 14:58:16 -0500

    first commit

--- src/main/java/com/restaurante/pizzeria/controller/ProductController.java
+++ src/main/java/com/restaurante/pizzeria/controller/ProductController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v2/products")
 public class ProductController {
